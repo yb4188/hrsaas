c930ee186f2904a061462159b2252e9e8b547a24
YouBing <2900856508@qq.com>
2023-01-19 22:48:12 +0800

    人力资源项目初始化

---
+++ src/store/modules/user.js
@@ -0,0 +1,14 @@
+/*
+ * @Author: YouBing 2900856508@qq.com
+ * @Date: 2023-01-19 21:28:56
+ * @LastEditors: YouBing 2900856508@qq.com
+ * @LastEditTime: 2023-01-19 21:53:06
+ * @FilePath: \vue_project_hrsaas\vue-admin-template\src\store\modules\user.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+export default {
+  namespaced: true,
+  actions: {},
+  mutations: {},
+  state: {}
+}
